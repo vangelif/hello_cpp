f256d2b72b45e8ddc651ea3ea96d8ce3bae5ad54
Vangelis <ev.fotiades@yahoo.com>
2023-04-05 23:07:52 +0300

    add lucky number

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
+    int a{30};
+    cout<< "the lucky number is: " << a<<endl;
+    cout<< "hello world!" <<endl;
+    cout<< "hello world!" <<endl;
     cout<< "hello world!" <<endl;
     return 0;
 }
